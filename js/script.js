9e1ba23c6dabcc2e649471548babe6d95b18a8d8
Donna Richards <15822195+DonnaRichards@users.noreply.github.com>
2020-12-17 17:46:31 +1100

    Completed project ready for initial submission

--- js/script.js
+++ js/script.js
@@ -10,33 +10,52 @@ project 1 - A Random Quote Generator
 
 // see separate js file quotes.js
 
+// Random number in range 0-255 used for colour settings
 const randomColour = () => Math.floor(Math.random() * 256);
 
+// RGB colour string in CSS format 
 const randomRGB = () => `rgb( ${randomColour()}, ${randomColour()}, ${randomColour()} )`;
+
+// Array containing indexes of quotes that have been displayed.  To keep track
+// so all quotes will be displayed before any repeat.
+let quotesDisplayed = [];
 
 /***
  * `getRandomQuote` function
+ * 
+ * @param None
+ * @returns quotes object
+ * 
+ * Select and return one quote to display.
+ * Randomly selected, but ensure a new quote is picked until all quotes have been
+ * displayed
 ***/
 
-let quotesUndisplayed = [];
-
 function getRandomQuote() {
-  if (quotesUndisplayed.length === quotes.length) {
-	  quotesUndisplayed = [];
+  // if size (length) of quotesDisplayed array equals that of quotes array, all quotes
+  // have been displayed.  So reset the quotesDisplayed array and start over.
+  if (quotesDisplayed.length === quotes.length) {
+	  quotesDisplayed = [];
   }
+  // pick random quote to display but not one that has already been displayed by
+  // ensuring that the random index picked not already in the quotesDisplayed array.
   let randomValue;
   do {
     randomValue = Math.floor(Math.random() * quotes.length);
-  } while ( ! quotesUndisplayed.includes(randomValue) );
-  quotesUndisplayed.push(randomValue);
-  console.log(randomValue);
+  } while ( quotesDisplayed.includes(randomValue) );
+
+  quotesDisplayed.push(randomValue);
   return quotes[randomValue];
 }
 
 /***
  * `printQuote` function
+ * 
+ * @param None
+ * @returns None
+ * 
+ * HTML formatting and display to webpage of one quote
 ***/
-
 
 function printQuote() {
   const quote = getRandomQuote();
@@ -54,11 +73,22 @@ function printQuote() {
     quoteHTML += `</p><p><span class="tags">${quote.tags.join(', ')}</span>`;
   }
   quoteHTML += `</p>`;
+  // Set background colour to a random RGB value
   document.getElementById('quote-box').style.backgroundColor = randomRGB();
+  // display the quote on the webpage
   document.getElementById('quote-box').innerHTML = quoteHTML;
 }
 
+// initial call of printQuote, done so one of my quotes will display immediately 
+// rather than the default quote in the base index.html
 printQuote();
+
+// Time in milliseconds between auto-refresh of quotes (= 10 seconds)
+const refreshInterval = 10000;
+
+// update the quote displayed at regular intervals per above refreshInterval variable.
+window.setInterval(printQuote, refreshInterval);
+
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
